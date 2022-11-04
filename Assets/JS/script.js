11a4b80055b13e3b68aa375a79500feb451cf4b1
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-11-03 21:47:11 -0400

    first commit added html,css, and js file

---
+++ Assets/JS/script.js
@@ -0,0 +1,15 @@
+
+
+var timeDisplayEl = $('#timeDisplay');
+
+
+
+
+
+function displayTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+  displayTime();
+  setInterval(displayTime, 1000);
